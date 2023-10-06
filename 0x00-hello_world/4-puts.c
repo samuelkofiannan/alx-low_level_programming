7e45fde52d49df6a6e347c6fc0e003566398c649
Kofi <qouphieligal@gmail.com>
2023-10-06 09:19:53 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+ * main - this uses put funtion
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
