848780a923c29dabec1d25ad65c717f64dba2bb3
Kofi <qouphieligal@gmail.com>
2024-01-23 15:16:10 -0800

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: final result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two integers
+ * @a: first int
+ * @b: second int
+ * Return: divided result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ * Return: final result
+ */
+
+int mod(int a, int b)
+{
+	return (a & b);
+}
